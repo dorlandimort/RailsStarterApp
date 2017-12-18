8db248c34cdc1a14dd43aa87c7a0326ce1b26ca9
dorlandimort <dorlandimort@gmail.com>
2017-12-18 13:24:19 -0600

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,10 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
-  # Disable serving static files from the `/public` folder by default since
+  # Disable serving static files from the `/public` folder by default sinceicons/glyphicons/glyphicons-halflings-regular.eot
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -29,8 +30,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
